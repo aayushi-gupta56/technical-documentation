26807d31b4193c14101b2d066a69635daed19ed1
ayushiGupta <aaaayushi85@gmail.com>
2021-07-12 17:30:44 +0530

    Added final onclick() event

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const menubar= document.querySelector('.menu');
 const btn = document.getElementsByClassName('smartphone-menu-trigger')[0];
+const li = menubar.getElementsByTagName('LI');
+
+for(const item of li){
+    item.addEventListener('click', function(event){
+        menubar.classList.remove('active');
+    });
+}
+
 function myFunction(e){
-    e.preventDefault();
-    (function(){
-        toggleClass('active');
-        })();
-        function toggleClass (className){
-          var active=menubar.classList.contains(className);
+    var active=menubar.classList.contains('active');
            if(active){
-                menubar.classList.remove(className);
+                menubar.classList.remove('active');
                 btn.style.background = "orangered";
            }
           else{
-                menubar.classList.add(className);
+                menubar.classList.add('active');
                 btn.style.background = "black";
           }
-         }
-     }
+}
